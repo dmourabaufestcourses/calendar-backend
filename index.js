de3d2e96c1d8537132674d4db1333d21e8bb2be4
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2025-06-17 19:35:12 -0300

    add express, first route

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo!");
+const express = require("express");
+const PORT = 4000;
+
+//crear el servidor de express
+const app = express();
+
+//Rutas
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+//escuchar peticiones
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto: ${PORT}`);
+});
