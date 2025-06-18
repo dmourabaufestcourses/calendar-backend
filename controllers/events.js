8f2d55a8c11564ac8704b71547f6f82a63155641
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2025-06-18 17:17:56 -0300

    delete events

--- controllers/events.js
+++ controllers/events.js
@@ -33,10 +33,10 @@ const createEventController = async (req, res = response) => {
 
 const uptateEventController = async (req, res = response) => {
   const eventoId = req.params.id;
+  const uid = req.uid;
 
   try {
     const evento = await Evento.findById(eventoId);
-    const uid = req.uid;
 
     if (!evento) {
       return res.status(404).json({
@@ -75,10 +75,40 @@ const uptateEventController = async (req, res = response) => {
 };
 
 const deleteEventController = async (req, res = response) => {
-  res.status(200).json({
-    ok: true,
-    msg: "deleteEvent",
-  });
+  const eventoId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const evento = await Evento.findById(eventoId);
+
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Event not found.",
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "Has not permissions.",
+      });
+    }
+
+ 
+    await Evento.findByIdAndDelete(eventoId);
+
+    res.json({
+      ok: true,
+      eventoId,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Ups something went wrong.",
+    });
+  }
 };
 
 module.exports = {
