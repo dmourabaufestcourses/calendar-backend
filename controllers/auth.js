94a5ff909f872927160518d3823dd295c630396c
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2025-06-18 10:54:35 -0300

    add register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,40 @@
 const { response } = require("express"); // esto es solo para volver a tener el intellisense
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/Usuario");
 
-const registerController = (req, res = response) => {
-  res.status(201).json({
-    ok: true,
-    msg: "register",
-    user: req.body,
-  });
+const registerController = async (req, res = response) => {
+  const { email, password } = req.body;
+  try {
+    let usuario = await Usuario.findOne({ email });
+    console.log(usuario);
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Couldn't create the user",
+      });
+    }
+
+    usuario = new Usuario(req.body);
+
+    // encript password
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Ups, algo salio mal",
+    });
+  }
 };
 
 const loginController = (req, res = response) => {
--- database/config.js
+++ database/config.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECTION, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    //   useCreateIndex: true, // ya no funciona
-    });
+    await mongoose.connect(process.env.DB_CONNECTION);
 
     console.log("DB online");
     
